625d5a422bc733442090887a31f28d72c1c4e82f
jordanstarr <jordan_cook2@baylor.edu>
2021-11-08 16:15:25 -0600

    butt

---
+++ workspace/report4/platform.tcl
@@ -0,0 +1,18 @@
+# 
+# Usage: To re-create this platform project launch xsct with below options.
+# xsct C:\Users\Jordan\OneDrive\College\WorkSpace\report4\platform.tcl
+# 
+# OR launch xsct and run below command.
+# source C:\Users\Jordan\OneDrive\College\WorkSpace\report4\platform.tcl
+# 
+# To create the platform in a different location, modify the -out option of "platform create" command.
+# -out option specifies the output directory of the platform project.
+
+platform create -name {report4}\
+-hw {C:\Users\Jordan\OneDrive\College\Engineering\System on Chip\Class Report 4\Class_report_4\report4.xsa}\
+-proc {microblaze_I} -os {standalone} -out {C:/Users/Jordan/OneDrive/College/WorkSpace}
+
+platform write
+platform generate -domains 
+platform active {report4}
+platform generate
---
+++ workspace/report4part2/platform.tcl
@@ -0,0 +1,18 @@
+# 
+# Usage: To re-create this platform project launch xsct with below options.
+# xsct C:\Users\Jordan\OneDrive\College\WorkSpace\report4part2\platform.tcl
+# 
+# OR launch xsct and run below command.
+# source C:\Users\Jordan\OneDrive\College\WorkSpace\report4part2\platform.tcl
+# 
+# To create the platform in a different location, modify the -out option of "platform create" command.
+# -out option specifies the output directory of the platform project.
+
+platform create -name {report4part2}\
+-hw {C:\Users\Jordan\OneDrive\College\Engineering\System on Chip\Class Report 4\Class_report_4\report4part2.xsa}\
+-proc {microblaze_I} -os {standalone} -out {C:/Users/Jordan/OneDrive/College/WorkSpace}
+
+platform write
+platform generate -domains 
+platform active {report4part2}
+platform generate
